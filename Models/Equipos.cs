022a856de14aefedbeb06800de054e8d160578c3
MARTIN EZEQUIEL RABER <47436029@ort>
2023-05-04 16:01:22 -0300

    Recien empezado

---
+++ Models/Equipos.cs
@@ -0,0 +1,9 @@
+public static class Equipos
+{
+    public static List<string> ListaEquipos{get;private set;}
+    public static List<string> ListaMedias{get;private set;}
+    public static List<string> ListaPantalones{get;private set;}
+    public static List<string> ListaCamisetas{get;private set;}
+    public static Dictionary<string, Indumentaria> EquiposIndumentaria {get;private set;}
+
+}
